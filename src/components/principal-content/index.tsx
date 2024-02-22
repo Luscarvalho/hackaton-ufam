7bbfb8fd14660ce84a846e17c13fa61a0373f282
LINDO <lilienep02@gmail.com>
2024-02-22 10:46:20 -0400

    add conteudo principal

---
+++ src/components/principal-content/index.tsx
@@ -0,0 +1,20 @@
+import PrincipalImg from "../../assets/images/image-web-3-desktop.jpg"
+
+export default function PrincipalContent() {
+    return (
+        <>
+            <div className="flex flex-wrap">
+                <img className="" src={PrincipalImg} alt="imagen principal" />
+                <div className="w-1/2">
+                    <h1 className="font-bold text-3xl">The Bright Future of Web 3.0?</h1>
+                </div>
+                <div className="w-1/2 flex flex-col gap-5">
+                    <p> We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?s</p>
+                    <div>
+                        <a className="p-2 bg-black text-white">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
